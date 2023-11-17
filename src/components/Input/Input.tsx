83654bb4f2bb94c13c760a7e631a2f0f5701b5ac
Berke <forcoding2022v2@gmail.com>
2023-11-17 16:53:37 +0300

    Up

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -7,14 +7,15 @@ interface InputProps {
     iconName: string;
     placeText: string;
     value: string;
+    security: boolean;
     onChangeText: (text: string) => void;
 }
 
-function Input({iconName, placeText,value,onChangeText}: InputProps) {
+function Input({iconName, placeText,value,security,onChangeText}: InputProps) {
     return (
         <View style={styles.box} >
             <Icon name={iconName} size={18} color={"black"} style={styles.icon} />
-            <TextInput placeholder={placeText} style={styles.input} value={value} onChangeText={onChangeText} />
+            <TextInput placeholder={placeText} style={styles.input} value={value} onChangeText={onChangeText} secureTextEntry={security} />
         </View>
     )
 };
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -46,12 +46,18 @@ function Login({ navigation }) {
     return (
         <SafeAreaView style={{ flex: 1 }} >
             <View style={styles.container} >
+                <Text style={{ fontSize: 18, paddingBottom: 20 }} >Hoşgeldiniz Lütfen Giriş Yapınız</Text>
                 <Image source={require("../../images/logo.png")} style={styles.logo} />
-                <LoginInput iconName={"person"} placeText={"Kullanıcı Adı"} value={username} onChangeText={(text) => setUsername(text)} />
-                <LoginInput iconName={"key"} placeText={"Şifre"} value={password} onChangeText={(text) => setPassword(text)} />
+                <LoginInput iconName={"person"} placeText={"Kullanıcı Adı"} value={username} security={false} onChangeText={(text) => setUsername(text)} />
+                <LoginInput iconName={"key"} placeText={"Şifre"} value={password} security={true} onChangeText={(text) => setPassword(text)} />
                 <TouchableOpacity onPress={handleLogin} style={styles.box}>
                     <Text style={styles.text} >Giriş Yap</Text>
                 </TouchableOpacity>
+                <View style={{ flexDirection: "row" }} >
+                    <Text>Henüz Hesabın Yok Mu? </Text>
+                    <Text style={{ color: "#ff5a00" }} >Kaydol!</Text>
+                </View>
+
             </View>
         </SafeAreaView>
     )
